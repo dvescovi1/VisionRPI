6f6b11af4c19aa96afcb40fd94a61f133fbbb31f
David Vescovi <youremail@yourdomain.com>
2022-05-11 23:39:22 +0000

    iot

--- modules/CameraCapture/app/main.py
+++ modules/CameraCapture/app/main.py
@@ -21,6 +21,44 @@ from tflite_support.task import core
 from tflite_support.task import processor
 from tflite_support.task import vision
 
+from azure.iot.device import IoTHubModuleClient, Message
+
+
+# global counters
+SEND_CALLBACKS = 0
+
+
+def send_to_Hub_callback(strMessage):
+    message = Message(bytearray(strMessage, 'utf8'))
+    hubManager.send_message_to_output(message, "output1")
+
+# Callback received when the message that we're forwarding is processed.
+
+class HubManager(object):
+
+    def __init__(
+            self,
+            messageTimeout,
+            verbose):
+        '''
+        Communicate with the Edge Hub
+
+        :param int messageTimeout: the maximum time in milliseconds until a message times out. The timeout period starts at IoTHubClient.send_event_async. By default, messages do not expire.
+        :param IoTHubTransportProvider protocol: Choose HTTP, AMQP or MQTT as transport protocol.  Currently only MQTT is supported.
+        :param bool verbose: set to true to get detailed logs on messages
+        '''
+        self.messageTimeout = messageTimeout
+        self.client = IoTHubModuleClient.create_from_edge_environment()
+        #self.client.set_option("messageTimeout", self.messageTimeout)
+        #self.client.set_option("product_info", "edge-camera-capture")
+        #if verbose:
+        #    self.client.set_option("logtrace", 1)  # enables MQTT logging
+
+    def send_message_to_output(self, event, outputQueueName):
+        self.client.send_message_to_output(event, outputQueueName)
+        global SEND_CALLBACKS
+        SEND_CALLBACKS += 1
+
 
 def runDetect(model: str, camera_id: int, width: int, height: int, num_threads: int,
         enable_edgetpu: bool) -> None:
