85de3dfa34c9527881b9f027389d1e7370e5eb1a
David Vescovi <youremail@yourdomain.com>
2022-05-15 03:10:56 +0000

    tweak

--- modules/CameraCapture/app/main.py
+++ modules/CameraCapture/app/main.py
@@ -1,17 +1,3 @@
-# Copyright 2021 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""Main script to run the object detection routine."""
 import os
 import sys
 import time
@@ -90,9 +76,6 @@ def runDetect(model: str, maxObjects: int, scoreThresholdPct: int, videoPath: st
   vs = None
   isWebcam = False
 
-  with CameraCapture(showVideo) as cameraCapture:
-    cameraCapture
-
   if (__IsInt(videoPath)):
     isWebcam = True
     vs = VideoStream(int(videoPath), width, height).start()
@@ -165,13 +148,6 @@ def runDetect(model: str, maxObjects: int, scoreThresholdPct: int, videoPath: st
     fps_text = 'FPS = {:.1f}'.format(fps)
     print(fps_text)
     print(counter)
-    # Stop the program if the ESC key is pressed.
-    if cv2.waitKey(1) == 27:
-      break
-
-#    if (__IsInt(videoPath)):
-#      cameraCapture.imageServer.close()
-#    cap.release()
 
 def main(
     debugy = False,
@@ -192,12 +168,6 @@ def main(
     #    import debugpy
     #    debugpy.listen(5678)
     #    debugpy.wait_for_client()  # blocks execution until client is attached
-    '''
-    Capture a camera feed, send it to processing and forward outputs to EdgeHub
-
-    :param str model: model file. Example: "efficientdet_lite0.tflite".
-    :param int cameraId: camera device path such as /dev/video0 or a test video file such as /TestAssets/myvideo.avi. /dev/video0 by default ("0")
-    '''
     try:
         print("\nPython %s\n" % sys.version)
         print("Camera Capture Azure IoT Edge Module. Press Ctrl-C to exit.")
@@ -222,6 +192,9 @@ def main(
         except Exception as iothub_error:
             print("Unexpected error %s from IoTHub" % iothub_error)
             return
+        global cameraCapture
+        with CameraCapture(showVideo) as cameraCapture:
+            cameraCapture
 
         runDetect(model,maxObjects,scoreThresholdPct,videoPath,frameWidth, frameHeight, numThreads,enableEdgeTPU, showVideo, bypassIot)
 
@@ -236,6 +209,13 @@ def __convertStringToBool(env):
     else:
         raise ValueError('Could not convert string to bool.')
 
+'''
+Capture a camera feed, send it to processing and forward outputs to EdgeHub
+
+:param str MODEL: model file. Example: "efficientdet_lite0.tflite".
+:param str VIDEO_PATH: camera device path such as /dev/video0 or a test video file such as /Test/myvideo.avi. /dev/video0 by default ("0")
+:param  etc..
+'''
 
 if __name__ == '__main__':
   try:
@@ -244,7 +224,6 @@ if __name__ == '__main__':
     MAX_OBJECTS = int(os.getenv('MAX_OBJECTS', 3))
     THRESHOLD_PCT = int(os.getenv('THRESHOLD_PCT', 30))
     VIDEO_PATH = os.getenv('VIDEO_PATH', "../test/AppleAndBanana.mp4")
-#    VIDEO_PATH = os.getenv('VIDEO_PATH', "0")
     FRAME_WIDTH = int(os.getenv('FRAME_WIDTH', 640))
     FRAME_HEIGHT = int(os.getenv('FRAME_HEIGHT', 480))
     NUM_THREADS = int(os.getenv('NUM_THREADS', 4))
